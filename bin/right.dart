bfe4e7c451b0f160fbf99921cc5f28ecdffba456
Sachin s p <sachinpremkumar26@gmail.com>
2022-01-02 04:33:49 +0530

    right traingle

--- bin/right.dart
+++ bin/right.dart
@@ -1,19 +1,14 @@
 import 'dart:io';
 
-//Left triangle star pattern
+//Right triangle star pattern
 void main(List<String> arguments) {
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 6; i++) {
+    for (int j = 2 * (6 - i); j >= 0; j--) {
+      stdout.write(' ');
+    }
     for (int j = 0; j <= i; j++) {
       stdout.write('* ');
     }
     stdout.writeln();
   }
 }
-
-
-//output
-// *
-// * *
-// * * *
-// * * * *
-// * * * * *
